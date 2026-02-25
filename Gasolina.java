cc00da6cc903a37770405d0d9a143f0b62712730
luis-guilhermee <luis.marianosenai@gmail.com>
2026-02-25 12:25:46 -0300

    segundo commit

--- Gasolina.java
+++ Gasolina.java
@@ -8,14 +8,14 @@ public class Gasolina {
 		Scanner ler = new Scanner (System.in);
 		double preco, distancia, litros, consumo, valor;
 		
-		System.out.println("Bem vindo ao medidor do consumo de gasolina");
+		System.out.println("Bem vindo ao medidor do consumo de gasolina!");
 		System.out.println("Digite o preço da gasolina: ");
 		preco = ler.nextDouble();
 		
 		System.out.println("Digite a distância total percorrida durante a viagem: ");
 		distancia = ler.nextDouble();
 		
-		System.out.println("Digite o consumo médio de gasolina por litro: ");
+		System.out.println("Digite o consumo médio de gasolina por km: ");
 		consumo = ler.nextDouble();
 		
 		litros = distancia / consumo;
